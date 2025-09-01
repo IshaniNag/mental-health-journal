88e5cf33da6c40e8f029442b2bf1bb28d3892992
Ishani Nag <ishaninag19@gmail.com>
2025-09-02 02:06:12 +0530

    Updated logos

--- frontend/src/templates/marketing-page/components/LogoCollection.tsx
+++ frontend/src/templates/marketing-page/components/LogoCollection.tsx
@@ -14,9 +14,9 @@ const darkModeLogos = [
 ];
 
 const lightModeLogos = [
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg',
+  'https://www.torxmedia.com/wp-content/uploads/2022/11/logoipsum-600x600.png',
+  'https://uploads-ssl.webflow.com/5c3a510a91db03828e568da1/5c3a6a8c652ba077e9640881_Logo%205%20Copy%209.svg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTMF3JIzEUr2XSCY1lJwfQfIZY2G4G-wom4Q&s',
   'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg',
   'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg',
   'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg',
